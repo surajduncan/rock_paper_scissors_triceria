6aa9d2b726347adf92295449430371585db0004d
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-16 23:58:24 +0530

    Update RockPaperScissors.java

--- program/RockPaperScissors.java
+++ program/RockPaperScissors.java
@@ -1,1 +1,22 @@
+package rockpaperscissors;
 
+/*
+ * RockPaperScissors runs the game "Rock, Paper, Scissors"
+ * 
+ * To start, just run the following:
+ * java RockPaperScissors.
+ */
+import java.util.Scanner;
+
+class RockPapersScissors {
+    
+    // Static variables
+    static Player p1;
+    static Player p2;
+    static Player[] players = new Player[2];
+    
+    static Scanner in;
+    static String msg1 = "Enter choice: ";
+    
+    static int limit = 3; // number of rounds
+    static boolean stop = false; // set to true to stop game
